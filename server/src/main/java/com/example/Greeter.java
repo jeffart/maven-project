779036a4032c7c1a7cdeec02208846cdf56f8fd3
jeffart <dekogha@gmail.com>
2021-09-20 02:26:34 +0200

    fix

--- server/src/main/java/com/example/Greeter.java
+++ server/src/main/java/com/example/Greeter.java
@@ -2,30 +2,29 @@ package com.example;
 
 
 /**
- * @author Intro
- *
- */
+* The Greeter program implements an application that
+* simply displays "Hello World!" to the standard output.
+*
+* @author  emd
+* @version 1.0
+* @since   2014-03-31 
+*/
 public class Greeter {
 
 
   public Greeter() {
 
   }
-
-
- /**
- * @param someone
- * @return string
- */
+ 
+  /**
+   * This method is used to print.
+   * a simple hello world.
+   *
+   * @param someone description de argument.
+   * @return string.
+   */
   public String greet(String someone) {
     return String.format("Hello, %s!", someone);
   }
   
-  
- /**
- * @return string
- */
-public String sayHello() {
-	  return "Hello";
-  }
 }
