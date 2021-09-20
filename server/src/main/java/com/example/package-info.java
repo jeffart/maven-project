d8260becfe4458a71bf4c57a0b57d8f08a8d3281
jeffart <dekogha@gmail.com>
2021-09-20 02:29:04 +0200

    new line

--- server/src/main/java/com/example/package-info.java
+++ server/src/main/java/com/example/package-info.java
@@ -1,12 +1,12 @@
 
 /**
- *  * This module is about impact of the final keyword on performance
+ * This module is about impact of the final keyword on performance.
  * <p>
  * This module explores  if there are any performance benefits from
  * using the final keyword in our code. This module examines the performance
  * implications of using final on a variable, method, and class level.
  * </p>
- * 
+ *
  * @since 1.0
  * @author emd
  * @version 1.1
