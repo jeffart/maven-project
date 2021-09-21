222c50e58c25a0b641d4f4aaf447720a77970375
jeffart <dekogha@gmail.com>
2021-09-21 10:12:05 +0200

    second greet

---
+++ server/src/main/java/com/example/GeetinG2.java
@@ -0,0 +1,31 @@
+package com.example;
+
+/**
+ * The Greeter program implements an application that
+ * simply displays "Hello World!" to the standard output.
+ *
+ * @author  emd
+ * @version 1.0
+ * @since   2014-03-31
+ */
+public class GeetinG2 {
+
+  /**
+   * this is the constructor.
+   */
+  public GeetinG2() {
+    super();
+    // TODO Auto-generated constructor stub
+  }
+
+  /**
+   * This method is used to print.
+   *
+   * @param world first arg
+   * @return string
+   */
+  public String greet(String world) {
+    return String.format("Hello, %s!", world);
+  }
+
+}
