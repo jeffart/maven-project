805c82e8667f64b54e6346c539021dffd8db10a0
jeffart <dekogha@gmail.com>
2021-09-21 09:18:02 +0200

    fix warnings

--- server/src/main/java/com/example/Greeter.java
+++ server/src/main/java/com/example/Greeter.java
@@ -2,32 +2,32 @@ package com.example;
 
 
 /**
-* The Greeter program implements an application that
-* simply displays "Hello World!" to the standard output.
-*
-* @author  emd
-* @version 1.0
-* @since   2014-03-31 
-*/
+ * The Greeter program implements an application that
+ * simply displays "Hello World!" to the standard output.
+ *
+ * @author  emd
+ * @version 1.0
+ * @since   2014-03-31
+ */
 public class Greeter {
 
-  /**
-   * This is the constructor.
-   *
-   */
-  public Greeter() {
+	/**
+	 * This is the constructor.
+	 *
+	 */
+	public Greeter() {
 
-  }
- 
-  /**
-   * This method is used to print.
-   * a simple hello world.
-   *
-   * @param someone description de argument.
-   * @return string.
-   */
-  public final String greet(final String someone) {
-    return String.format("Hello, %s!", someone);
-  }
-  
+	}
+
+	/**
+	 * This method is used to print.
+	 * a simple hello world.
+	 *
+	 * @param someone description de argument.
+	 * @return string.
+	 */
+	public final String greet(final String someone) {
+		return String.format("Hello, %s!", someone);
+	}
+
 }
